757317f54eef31e49619ef3577f614df2a63184b
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-02-05 18:58:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,4 +48,16 @@ class InteractiveRecord
     values.join(", ")
   end
 
+  def self.find_by_name(name)
+  sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+  DB[:conn].execute(sql)
+end
+
+def self.find_by(attribute_hash)
+  value = attribute_hash.values.first
+  formatted_value = value.class == Fixnum ? value : "'#{value}'"
+  sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+  DB[:conn].execute(sql)
+end
+
 end
